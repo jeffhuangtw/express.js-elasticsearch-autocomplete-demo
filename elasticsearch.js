7755d2a7946cccd717fa906c68baa6f81e96384a
Jeff Huang <jeffhuang@zappoint.com>
2017-07-03 17:45:42 +0800

    Apply elasticsearch 5.4 and fix issues

--- elasticsearch.js
+++ elasticsearch.js
@@ -1,19 +1,23 @@
 var elasticsearch = require('elasticsearch');
 
+if (!process.env.SEARCHBOX_SSL_URL) {
+  console.error('SEARCHBOX_SSL_URL not found!!! please enable Heroku searchbox plugin!!!');
+}
+
 var elasticClient = new elasticsearch.Client({
-    host: 'localhost:9200',
-    log: 'info'
+  host: process.env.SEARCHBOX_SSL_URL,
+  log: 'info'
 });
 
-var indexName = "randomindex";
+var indexName = 'randomindex';
 
 /**
 * Delete an existing index
 */
 function deleteIndex() {
-    return elasticClient.indices.delete({
-        index: indexName
-    });
+  return elasticClient.indices.delete({
+    index: indexName
+  });
 }
 exports.deleteIndex = deleteIndex;
 
@@ -21,9 +25,9 @@ exports.deleteIndex = deleteIndex;
 * create the index
 */
 function initIndex() {
-    return elasticClient.indices.create({
-        index: indexName
-    });
+  return elasticClient.indices.create({
+    index: indexName
+  });
 }
 exports.initIndex = initIndex;
 
@@ -31,62 +35,53 @@ exports.initIndex = initIndex;
 * check if the index exists
 */
 function indexExists() {
-    return elasticClient.indices.exists({
-        index: indexName
-    });
+  return elasticClient.indices.exists({
+    index: indexName
+  });
 }
 exports.indexExists = indexExists;
 
 function initMapping() {
-    return elasticClient.indices.putMapping({
-        index: indexName,
-        type: "document",
-        body: {
-            properties: {
-                title: { type: "string" },
-                content: { type: "string" },
-                suggest: {
-                    type: "completion",
-                    analyzer: "simple",
-                    search_analyzer: "simple",
-                    payloads: true
-                }
-            }
-        }
-    });
+  return elasticClient.indices.putMapping({
+    index: indexName,
+    type: 'document',
+    body: {
+      properties: {
+        title: { type: 'string' },
+        content: { type: 'string' },
+        title_suggest: { type: 'completion' }
+      }
+    }
+  });
 }
 exports.initMapping = initMapping;
 
 function addDocument(document) {
-    return elasticClient.index({
-        index: indexName,
-        type: "document",
-        body: {
-            title: document.title,
-            content: document.content,
-            suggest: {
-                input: document.title.split(" "),
-                output: document.title,
-                payload: document.metadata || {}
-            }
-        }
-    });
+  return elasticClient.index({
+    index: indexName,
+    type: 'document',
+    body: {
+      title: document.title,
+      content: document.content,
+      title_suggest: {
+        input: document.title.split(' ')
+      }
+    }
+  });
 }
 exports.addDocument = addDocument;
 
 function getSuggestions(input) {
-    return elasticClient.suggest({
-        index: indexName,
-        type: "document",
-        body: {
-            docsuggest: {
-                text: input,
-                completion: {
-                    field: "suggest",
-                    fuzzy: true
-                }
-            }
+  return elasticClient.suggest({
+    index: indexName,
+    body: {
+      suggest_title: {
+        text: input,
+        completion: {
+          field: 'title_suggest'
         }
-    })
+      }
+    }
+  });
 }
 exports.getSuggestions = getSuggestions;
--- routes/documents.js
+++ routes/documents.js
@@ -4,13 +4,13 @@ var router = express.Router();
 var elastic = require('../elasticsearch');
 
 /* GET suggestions */
-router.get('/suggest/:input', function (req, res, next) {
-  elastic.getSuggestions(req.params.input).then(function (result) { res.json(result) });
+router.get('/suggest/:input', function (req, res) {
+  elastic.getSuggestions(req.params.input).then(function (result) { res.json(result); });
 });
 
 /* POST document to be indexed */
-router.post('/', function (req, res, next) {
-  elastic.addDocument(req.body).then(function (result) { res.json(result) });
+router.post('/', function (req, res) {
+  elastic.addDocument(req.body).then(function (result) { res.json(result); });
 });
 
 module.exports = router;
